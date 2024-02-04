0521c57fd571294ddf9b1da489e49fbd62167ca1
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-02-04 01:39:52 -0600

    fixed about me modal

--- src/hooks/AboutMeModal.jsx
+++ src/hooks/AboutMeModal.jsx
@@ -3,10 +3,10 @@ import { SiExpress, SiPostgresql, SiSocketdotio } from "react-icons/si";
 
 const AboutMeModal = ({ onClose, children, aboutMeText , aboutMeModalClass, bgClass, presentationText, presentationClass, aboutMeClass}) => {
     return (
-      <div className={`z-40 ${aboutMeModalClass} w-[350px] sm:w-[500px] md:w-[700px] lg:w-[850px] mx-auto fixed inset-0 bg-none px-4 flex justify-center
+      <div className={`z-40 fixed inset-0 ${aboutMeModalClass} w-[350px] sm:w-[500px] md:w-[700px] lg:w-[850px] mx-auto fixed inset-0 bg-none px-2 flex justify-center
         items-center`}>
         <div className={`fixed inset-0 ${bgClass}`} onClick={onClose}></div>
-        <div className={`${aboutMeClass} flex flex-col md:flex-row justify-center items-center m-auto w-auto h-auto gap-4`}>
+        <div className={`${aboutMeClass} z-50 flex flex-col md:flex-row justify-center items-center m-auto w-auto h-auto gap-4`}>
             <div className={`text-xs sm:text-lg text-pretty gap-y-1 flex flex-col `}>
               <h1 className={`${presentationClass} text-sm sm:text-xl`}>{presentationText}</h1>
               <h1>{aboutMeText}</h1>
