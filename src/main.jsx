55f7e273bd7292f674a781bc7364f7ce65c3ff50
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-04-13 01:54:18 -0600

    Using new context

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { LanguageProvider } from "./Context/LanguageContext";
+import { ModalProvider } from "./Context/ModalContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-     <App />
-  </>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ModalProvider>
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
+  </ModalProvider>
+);
