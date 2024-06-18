9277527bf017f98f3b129fd3b07c9705d2e5a35a
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-06-17 22:05:01 -0600

    🪄 Add intended code and fix mobile design

--- src/Components/TechButton.jsx
+++ src/Components/TechButton.jsx
@@ -1,10 +1,13 @@
-const Buttons = ({buttonText, children}) => {
-    return (
-            <div className={`default-styles py-1 col-span-1 px-2 rounded-full border flex flex-row gap-x-2 items-center justify-center
-                            text-xs dark:border-black/50 dark:bg-black/90 bg-black text-white border-black`}>
-                {children}
-                {buttonText}
-            </div>
-    );
-  };
-  export default Buttons;
+const Buttons = ({ buttonText, children }) => {
+  return (
+    <div
+      className={`default-styles py-1 col-span-1 px-2 rounded-full border flex flex-row gap-x-2 items-center justify-center
+      text-[10px] md:text-xs dark:border-black/50 dark:bg-black/90 bg-black text-white/60 border-black hover:text-white dark:hover:border-white/50
+      hover:cursor-pointer hover:bg-black/80 hover:text-white/100 transition-all`}
+    >
+      {children}
+      {buttonText}
+    </div>
+  );
+};
+export default Buttons;
