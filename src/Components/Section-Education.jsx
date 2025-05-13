c885b9eb2fa948102ca3ac6a197f57839931330e
Hypeyiii <isaac.friaslns@uanl.edu.mx>
2025-05-12 22:29:41 -0600

    👏🏻Add new item on Experience section

--- src/Components/Section-Education.jsx
+++ src/Components/Section-Education.jsx
@@ -24,39 +24,89 @@ function Education() {
         </h2>
       </div>
       <Timeline className="text-black ml-[6px]">
-      <Timeline.Item>
+        <Timeline.Item>
           <Timeline.Point />
           <Timeline.Content className="md:gap-y-2 flex flex-col items-start justify-center">
             <Timeline.Time
               className={`text-xs md:text-xl text-black/80 dark:text-white/80`}
             >
               {[
-                isSpanish && "Agosto 2024 - Actualmente",
-                isEnglish && "August 2024 - Currently",
+                isSpanish && "Febrero 2025 - Actualmente",
+                isEnglish && "February 2025 - Currently",
               ]}
             </Timeline.Time>
             <Timeline.Title
               className={`text-base md:text-2xl text-dark dark:text-white`}
             >
-              {[
-                isSpanish && "Practicante de IT",
-                isEnglish && "IT Intern",
-              ]}
+              {[isSpanish && "Técnico en IT", isEnglish && "IT Technician"]}
             </Timeline.Title>
             <Timeline.Body
-              className={`text-sm md:text-lg text-black/70 dark:text-white/70`}
+              className={`text-xs md:text-sm text-black/70 dark:text-white/70`}
             >
               {[
                 isSpanish && (
-                  <p>
-                    Comencé a trabajar en el departamento de IT en la empresa de Polaris
-                    Industries.
-                  </p>
+                  <>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>Actualmente desempeñando el rol de Técnico
+                      de IT en Polaris Inc.
+                    </p>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>Gestión y mantenimiento de Active Directory
+                      (usuarios, grupos, políticas de grupo).
+                    </p>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>SCCM, distribución de software,
+                      actualizaciones, gestión de dispositivos.
+                    </p>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>Resolución de problemas de hardware y
+                      software (firmware, drivers, BIOS, Bitlocker, etc.).
+                    </p>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>Soporte a lineas de producción y estaciones
+                      de trabajo.
+                    </p>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>Seguimiento, documentación y resolución de
+                      incidencias utilizando ServiceNow.
+                    </p>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>Conocimiento en conceptos/herramientas
+                      relacionadas a Network (Cisco DNA, Putty, etc.).
+                    </p>
+                  </>
                 ),
                 isEnglish && (
-                  <p>
-                    I started working in the IT department at Polaris Industries.
-                  </p>
+                  <>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>Currently working as an IT Technician at
+                      Polaris Inc.
+                    </p>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>Management and maintenance of Active
+                      Directory (users, groups, group policies).
+                    </p>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>SCCM, software distribution, updates, device
+                      management.
+                    </p>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>Troubleshooting hardware and software
+                      (firmware, drivers, BIOS, Bitlocker, etc.).
+                    </p>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>Support for production lines and
+                      workstations.
+                    </p>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>Tracking, documentation and resolution of
+                      incidents using ServiceNow.
+                    </p>
+                    <p className="flex flex-row gap-x-2 ml-5">
+                      <span>-</span>Knowledge of Network-related concepts/tools
+                      (Cisco DNA, Putty, etc.).
+                    </p>
+                  </>
                 ),
               ]}
             </Timeline.Body>
@@ -69,9 +119,42 @@ function Education() {
               className={`text-xs md:text-xl text-black/80 dark:text-white/80`}
             >
               {[
-                isSpanish && "Agosto 2023",
-                isEnglish && "August 2023",
+                isSpanish && "Agosto 2024 - Febrero 2025",
+                isEnglish && "August 2024 - February 2025",
               ]}
+            </Timeline.Time>
+            <Timeline.Title
+              className={`text-base md:text-2xl text-dark dark:text-white`}
+            >
+              {[isSpanish && "Practicante de IT", isEnglish && "IT Intern"]}
+            </Timeline.Title>
+            <Timeline.Body
+              className={`text-sm md:text-lg text-black/70 dark:text-white/70`}
+            >
+              {[
+                isSpanish && (
+                  <p>
+                    Comencé a trabajar en el departamento de IT en la empresa de
+                    Polaris Industries.
+                  </p>
+                ),
+                isEnglish && (
+                  <p>
+                    I started working in the IT department at Polaris
+                    Industries.
+                  </p>
+                ),
+              ]}
+            </Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content className="md:gap-y-2 flex flex-col items-start justify-center">
+            <Timeline.Time
+              className={`text-xs md:text-xl text-black/80 dark:text-white/80`}
+            >
+              {[isSpanish && "Agosto 2023", isEnglish && "August 2023"]}
             </Timeline.Time>
             <Timeline.Title
               className={`text-base md:text-2xl text-dark dark:text-white`}
@@ -108,7 +191,10 @@ function Education() {
               className={`flex flex-row gap-x-1 items-center justify-center p-2 bg-black border border-white/20 rounded-xl
               transition-all duration-300 text-white/60 hover:text-white active:scale-105 active:duration-75 text-xs md:text-sm`}
             >
-              {[isSpanish && "Mira todos mis proyectos", isEnglish && "See all my projects"]}
+              {[
+                isSpanish && "Mira todos mis proyectos",
+                isEnglish && "See all my projects",
+              ]}
               <FaAngleRight />
             </a>
           </Timeline.Content>
